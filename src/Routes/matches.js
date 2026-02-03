fbeb6e92665ac85092ebefe58889572281a437db
SanManjiro <karmelkeke@gmail.com>
2026-02-03 14:16:47 +0100

    Making match endpoints

--- src/Routes/matches.js
+++ src/Routes/matches.js
@@ -1,6 +1,51 @@
-import {Router} from "express";
+import { Router } from "express";
+import {createMatchSchema, listMatchesQuerySchema} from "../validation/matches.js";
+import { getMatchStatus } from "../utils/match-status.js";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+import {desc} from "drizzle-orm";
 
 export const MatchesRouter = Router();
-MatchesRouter.get("/MatchesList", (req, res) =>{
-    res.status(200).json({message: "Matches List"});
+const MAX_LIMIT=100;
+MatchesRouter.get("/", async (req, res) => {
+ const parsed = listMatchesQuerySchema.safeParse(req.query);
+ if (!parsed.success) {
+   return res.status(400).json({ message: "Invalid Query Params", errors: parsed.error.errors });
+ }
+ const limit=Math.min(parsed.data.limit ?? 50 , MAX_LIMIT)
+  try {
+    const data = await db
+        .select()
+        .from(matches)
+        .orderBy(desc(matches.createdAt))
+        .limit(limit);
+    res.status(200).json({data});
+  }catch (e) {
+    res.status(500).json({ error: "Failed fetching match", details: e.message });
+  }
+  const match=db.select().from(matches).limit(limit);
+});
+
+MatchesRouter.post("/", async (req, res) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+  if (!parsed.success) {
+    return res.status(400).json({ message: "Invalid Payload", errors: parsed.error.errors });
+  }
+
+  const { startTime, endTime, homeScore, awayScore } = parsed.data;
+
+  try {
+    const [event] = await db.insert(matches).values({
+      ...parsed.data,
+      startTime: new Date(startTime),
+      endTime: new Date(endTime),
+      homeScore: homeScore ?? 0,
+      awayScore: awayScore ?? 0,
+      status: getMatchStatus(startTime, endTime)
+    }).returning();
+
+    res.status(201).json({data:event});
+  } catch (e) {
+    res.status(500).json({ error: "Database error", details: e.message });
+  }
 });
